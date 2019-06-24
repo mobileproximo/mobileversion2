5ae671eca49d6fd29c5ef389b74b345730594924
mobileproximo <proximoatps@gmail.com>
2019-06-24 19:17:24 +0000

    Ajout time out à 90s

--- src/providers/service/service.ts
+++ src/providers/service/service.ts
@@ -57,7 +57,7 @@ export class ServiceProvider {
       console.log(url);
       console.log(body);
       this.http.setDataSerializer("json");
-      this.http.setRequestTimeout(30);
+      this.http.setRequestTimeout(90);
       this.http.setSSLCertMode("nocheck");
       return this.http.post(url, body, headers);
     }
